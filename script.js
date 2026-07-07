69b7d1b6f1f4b1f15db0b6e3012aae73e81d97dd
intanhalu <intan.syaza@gmail.com>
2026-07-07 21:35:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -62,14 +62,14 @@ const timeOptions = [
 ];
 
 // Shuffle array function
-function shuffleArray(array) {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-}
+//function shuffleArray(array) {
+  //  const shuffled = [...array];
+   // for (let i = shuffled.length - 1; i > 0; i--) {
+      //  const j = Math.floor(Math.random() * (i + 1));
+     //   [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  //  }
+  //  return shuffled;
+//}
 
 // Generate questions for Kenali Nombor (Recognize Numbers)
 //function generateKenaliNomborQuestions(difficulty) {
